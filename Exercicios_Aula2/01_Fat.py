7da9fa89ecf0ca38fa32fd0509aa6f08fd3b6ba6
toledorp <roge.pupo.toledo@gmail.com>
2026-03-15 19:35:44 -0300

    Exercicio em phyton

---
+++ Exercicios_Aula2/01_Fat.py
@@ -0,0 +1,8 @@
+peso1 = float(input("Digite o peso da primeira pessoa: "))
+peso2 = float(input("Digite o peso da segunda pessoa: "))
+if peso1 > peso2:
+    print("A primeira pessoa é mais pesada.")
+elif peso2 > peso1:
+    print("A segunda pessoa é mais pesada.")
+else:    
+    print("As duas pessoas têm o mesmo peso.")
